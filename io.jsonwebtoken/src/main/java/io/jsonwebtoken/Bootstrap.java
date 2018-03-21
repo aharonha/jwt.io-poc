7f93aba01eef104c4c026e1a8b8d2ba1742d889c
aharonha <aharon@marketspulse.com>
2018-03-21 16:02:33 +0200

    keys on store are now stored as base 64
    in order to read certificates which contain multiline plain-text.

--- io.jsonwebtoken/src/main/java/io/jsonwebtoken/Bootstrap.java
+++ io.jsonwebtoken/src/main/java/io/jsonwebtoken/Bootstrap.java
@@ -1,60 +1,83 @@
 package io.jsonwebtoken;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.Key;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-import java.security.spec.X509EncodedKeySpec;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.function.BiFunction;
+
+import javax.crypto.spec.SecretKeySpec;
 
 public class Bootstrap {
 
   private static final String TENENT_ID = "tenentId";
 
   public static void main(String[] args) throws Exception {
+    final CertificateFactory cf = CertificateFactory.getInstance("X.509");
 
 
-    Map<String, byte[]> keyStore = new HashMap<>();
+    Map<String, byte[]> secretKeyStore = new HashMap<>();
     Properties properties = System.getProperties();
     String keystorePath = properties.getProperty("keystore.path");
     Properties keyFile = new Properties();
     try {
       keyFile.load(new FileReader(new File(keystorePath)));
       keyFile
-          .forEach((Object key, Object value) -> keyStore.put(String.valueOf(key), String.valueOf(value).getBytes()));
+          .forEach((Object key, Object value) -> secretKeyStore.put(String.valueOf(key),
+              Base64.getDecoder().decode(String.valueOf(value))));
     } catch (Exception ignoredException) {
       throw ignoredException;
     }
 
+
+
     JwtParser parser = Jwts.parser();
 
-    SigningKeyResolver signingKeyResolver = new SigningKeyResolverAdapter() {
-      @Override
-      public byte[] resolveSigningKeyBytes(JwsHeader header, Claims claims) {
-        return keyStore.get(header.getKeyId());
+    SigningKeyResolver signingSecretKeyResolver = new SigningKeyResolver() {
+
+      public Key resolveSigningKey(JwsHeader<?> header) {
+        SignatureAlgorithm alg = SignatureAlgorithm.forName(header.getAlgorithm());
+        byte[] keyBytes = secretKeyStore.get(header.getKeyId());
+        if (alg.isHmac()) {
+          return new SecretKeySpec(keyBytes, alg.getJcaName());
+        } else if (alg.isRsa()) {
+          try {
+            return cf.generateCertificate(new ByteArrayInputStream(keyBytes)).getPublicKey();
+          } catch (CertificateException ignoredException) {
+            return null;
+          }
+        } else if (alg.isEllipticCurve()) {
+          return null;
+        }
+        throw new IllegalArgumentException("Unknown algorithm");
       }
 
       @Override
-      public byte[] resolveSigningKeyBytes(JwsHeader header, String payload) {
-        return this.resolveSigningKeyBytes(header, (Claims) null);
+      public Key resolveSigningKey(JwsHeader header, Claims claims) {
+        return resolveSigningKey(header);
+      }
+
+      @Override
+      public Key resolveSigningKey(JwsHeader header, String plaintext) {
+        return resolveSigningKey(header);
       }
     };
 
+    secretKeyStore.keySet().forEach(System.out::println);
 
     for (String token : args) {
-      Jws<Claims> jwt = parser.setSigningKeyResolver(signingKeyResolver).parseClaimsJws(token);
-      System.out.println(jwt);
+      Jws<Claims> jwt;
+      try {
+        jwt = parser.setSigningKeyResolver(signingSecretKeyResolver).parseClaimsJws(token);
+        System.out.println(jwt);
+      } catch (SignatureException ignoredException) {
+        ignoredException.printStackTrace();
+      }
     }
   }
 }
